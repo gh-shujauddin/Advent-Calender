238baf5874ee2a166cb7c82b2cb67e42fcc4a3eb
gh-shujauddin <mshuja.uq@gmail.com>
2024-01-05 23:40:01 +0530

    typescript alias config

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,27 @@
 import { Stack } from "expo-router";
+import { useEffect } from 'react';
+import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
+import {AmaticSC_400Regular, AmaticSC_700Bold} from '@expo-google-fonts/amatic-sc';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+    const [fontLoaded, fontError] = useFonts({
+        Inter: Inter_900Black,
+        Amatic: AmaticSC_400Regular,
+        AmaticBold: AmaticSC_700Bold
+      });
+    
+      useEffect(() => {
+        if(fontLoaded || fontError) {
+          SplashScreen.hideAsync();
+        }
+      }, [fontLoaded, fontError]);
+    
+      if(!fontLoaded && !fontError) {
+        return null;
+      }
     return (
         <Stack screenOptions={{}}>
             <Stack.Screen name="index" options={{title: 'Advent Calender'}}/>
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,31 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
-import DayListItem from '../../src/components/core/DayListItem';
-import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
-import {AmaticSC_400Regular, AmaticSC_700Bold} from '@expo-google-fonts/amatic-sc';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-
-SplashScreen.preventAutoHideAsync();
+import DayListItem from '@components/core/DayListItem';
 
 const days = [...Array(24)].map((value,index) => index+1)
 
 export default function HomeScreen() {
-  const [fontLoaded, fontError] = useFonts({
-    Inter: Inter_900Black,
-    Amatic: AmaticSC_400Regular,
-    AmaticBold: AmaticSC_700Bold
-  });
-
-  useEffect(() => {
-    if(fontLoaded || fontError) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontLoaded, fontError]);
-
-  if(!fontLoaded && !fontError) {
-    return null;
-  }
+  
   return (
     <View style={styles.container}>
 
