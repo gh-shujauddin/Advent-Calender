90b15b816d5da90653a19c4890421ff4fc8da36f
gh-shujauddin <mshuja.uq@gmail.com>
2024-01-09 16:37:03 +0530

    Onboarding Screen Completed

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,31 +1,37 @@
 import React from "react";
 import { Stack } from "expo-router";
 import { useEffect } from 'react';
-import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
-import {AmaticSC_400Regular, AmaticSC_700Bold} from '@expo-google-fonts/amatic-sc';
+import { useFonts, Inter_600SemiBold, Inter_700Bold, Inter_400Regular, Inter_900Black } from '@expo-google-fonts/inter';
+import { AmaticSC_400Regular, AmaticSC_700Bold } from '@expo-google-fonts/amatic-sc';
 import * as SplashScreen from 'expo-splash-screen';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-    const [fontLoaded, fontError] = useFonts({
-        Inter: Inter_900Black,
-        Amatic: AmaticSC_400Regular,
-        AmaticBold: AmaticSC_700Bold
-      });
-    
-      useEffect(() => {
-        if(fontLoaded || fontError) {
-          SplashScreen.hideAsync();
-        }
-      }, [fontLoaded, fontError]);
-    
-      if(!fontLoaded && !fontError) {
-        return null;
-      }
-    return (
-        <Stack screenOptions={{}}>
-            <Stack.Screen name="index" options={{title: 'Advent Calender'}}/>
-        </Stack>
-    );
+  const [fontLoaded, fontError] = useFonts({
+    Inter: Inter_400Regular,
+    InterSemi: Inter_600SemiBold,
+    InterBold: Inter_700Bold,
+    InterBlack: Inter_900Black,
+    Amatic: AmaticSC_400Regular,
+    AmaticBold: AmaticSC_700Bold
+  });
+
+  useEffect(() => {
+    if (fontLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontLoaded, fontError]);
+
+  if (!fontLoaded && !fontError) {
+    return null;
+  }
+  return (
+    <GestureHandlerRootView style={{flex: 1}}>
+      <Stack screenOptions={{}}>
+        <Stack.Screen name="index" options={{ title: 'Advent Calender' }} />
+      </Stack>
+    </GestureHandlerRootView>
+  );
 }
